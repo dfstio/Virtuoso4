4b3ff48e783d678618fe18fc701d70a4cd4c76b5
Atul Midha <atul.midda@g-axon.com>
2020-01-15 18:15:19 +0530

    Created Master branch

---
+++ config-overrides.js
@@ -0,0 +1,23 @@
+const path = require('path');
+const {override, fixBabelImports, addLessLoader} = require('customize-cra');
+
+const overrideProcessEnv = value => config => {
+  config.resolve.modules = [
+    path.join(__dirname, 'src')
+  ].concat(config.resolve.modules);
+  return config;
+};
+
+module.exports = override(
+  fixBabelImports('import', {
+    libraryName: 'antd',
+    libraryDirectory: 'es',
+    style: 'css',
+  }),
+  addLessLoader({
+    javascriptEnabled: true,
+  }),
+  overrideProcessEnv({
+    VERSION: JSON.stringify(require('./package.json').version),
+  })
+);
