02e8f37394f7ebbbbc5e6bfa27dbb24431045702
Benjamin Herald <bherald@pm.me>
2021-09-15 00:52:37 +0300

    algolia 2

---
+++ src/functions/algolia-init-background.js
@@ -0,0 +1,41 @@
+const { initAlgoliaTokens} = require("../serverless/contract");
+
+
+exports.handler = async(event, context) => {
+    //const { name = "Anonymous" } = event.queryStringParameters;
+
+
+
+        // check for POST
+    if (event.httpMethod !== "POST") {
+        return {
+            statusCode: 400,
+            body: "algolia-init-background: You are not using a http POST method for this endpoint.",
+            headers: { Allow: "POST" },
+        };
+    }
+
+    try {
+
+        // parse form data
+        const body = JSON.parse(event.body);
+        let force = true;
+        if( body.force != undefined && body.force == false) force = false;
+
+        let result = await initAlgoliaTokens(force);
+        console.log("algolia-init-background: ", result.toString(), " tokens");
+
+        // return success
+        return {
+            statusCode: 200,
+            body: JSON.stringify({
+                success: true,
+                data: result,
+            }),
+        };
+
+    } catch (error) {
+       console.error("algolia-init-background: ", error);
+    }
+
+};
---
+++ src/serverless/algolia.js
@@ -0,0 +1,43 @@
+const algoliasearch = require('algoliasearch');
+const client = algoliasearch('KJYWN9CKS8', 'e362c0f63b9afb700db75abfafecb1aa');
+const index = client.initIndex('virtuoso');
+
+async function alWriteToken(tokenId, token, contract, chainId)
+{
+
+        // create item to insert
+        const params = {
+                objectID: Number(tokenId),
+                contract: contract,
+                chainId: chainId
+                tokenId: Number(tokenId),
+                owner: token.owner,
+                name: token.name,
+                description: token.description,
+                onSale: token.onSale,
+                price: token.sale.price,
+                category: "music",
+                image: token.image,
+                token: token,
+                updated: Date.now()
+        };
+        console.log("Algolia write ",  params);
+
+   try {
+
+        let result = await index.saveObjects(params, { autoGenerateObjectIDIfNotExist: false });
+
+
+    } catch (error) {
+
+       console.log(" alWriteToken error: ", error);
+       return error;
+    }
+
+}
+
+
+
+module.exports = {
+    alWriteToken
+}
