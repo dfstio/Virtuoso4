65c99cec3d4abc9b85ab9762ba7fdc4c55d3cadd
Benjamin Herald <bherald@pm.me>
2021-11-06 01:45:11 +0300

    images

--- src/routes/Mint/Butterflies.js
+++ src/routes/Mint/Butterflies.js
@@ -1,6 +1,6 @@
 import React, {useState, useEffect} from "react";
-import {Card, Button, Row, Col, Select} from "antd";
-
+import {Card, Button, Row, Col, Select, Slider} from "antd";
+import Jimp from 'jimp';
 const {Meta} = Card;
 const { Option } = Select;
 const DEBUG = true;
@@ -77,35 +77,60 @@ const MintButterfly = () => {
     const [price, setPrice] = useState(700);
     const [left, setLeft] = useState(5);
     const [right, setRight] = useState(6);
+    const [slider, setSlider] = useState(50);
     const [minting, setMinting] = useState(false);
+    const [disabled, setDisabled] = useState(false);
     const [mintDisabled, setMintDisabled] = useState(false);
 
       useEffect(() => {
             async function changeNumbers() {
 
                  if( DEBUG) console.log("MintButterfly numbers: ", left, right);
-                 const newPrice = prices[rare[left]]+prices[rare[right]];
-                 if( price !== newPrice) setPrice(newPrice);
-                 setTitle(butterflies[left]+"-"+butterflies[right]);
-                 setDescription(
+
+                 if( left !== right)
+                     {
+                     const newPrice = prices[rare[left]]*(1+(100-slider)/100)+prices[rare[right]]*(1+slider/100);
+                     const newPrice1 = newPrice.toFixed(0);
+                     if( price !== newPrice) setPrice(newPrice1);
+                     setTitle(butterflies[left]+"-"+butterflies[right]);
+                     if( disabled) setDisabled(false);
+                     setDescription(
 `Эта уникальная бабочка скрещена из двух видов:
 ${names[left]} - 50%
 ${names[right]}  - 50%`);
+                     		let image1 = await Jimp.read("https://content.nftvirtuoso.io/image/batterflies/" + left.toString() + ".jpg");
+                        let image2 = await Jimp.read("https://content.nftvirtuoso.io/image/batterflies/" + right.toString() + ".jpg");
+                        image1.composite(image2, 0, 0, {
+                         mode: Jimp.BLEND_SCREEN,
+                           opacitySource: slider/100,
+                           opacityDest: 1
+                         });
+                         //const newImage = await image1.getBase64Async(Jimp.MIME_JPEG);
+                         //setImage(newImage);
 
-
-
+                 }
+                 else
+                 {
+                      setPrice(prices[rare[left]]);
+                      setTitle(butterflies[right]);
+                      setDescription(names[left]);
+                      if( !disabled) setDisabled(true);
+                 };
         }
       changeNumbers()
       },[left, right]);
 
       useEffect(() => {
-            async function start() {
+            async function changePrice() {
 
-                 if( DEBUG) console.log("MintButterfly: ", names);
+                 if( DEBUG) console.log("MintButterfly slider: ", slider);
+                 const newPrice = prices[rare[left]]*(1+(100-slider)/100)+prices[rare[right]]*(1+slider/100);
+                 const newPrice1 = newPrice.toFixed(0);
+                 if( price !== newPrice) setPrice(newPrice1);
 
         }
-      start()
-      },[]);
+      changePrice()
+      },[slider]);
 
 
 
@@ -117,14 +142,22 @@ ${names[right]}  - 50%`);
   };
 
   function handleChangeLeft(value) {
-    if(DEBUG)  console.log("Select left: ", value.value); // { value: "lucy", key: "lucy", label: "Lucy (101)" }
+    //if(DEBUG)  console.log("Select left: ", value.value); // { value: "lucy", key: "lucy", label: "Lucy (101)" }
     setLeft(value.value);
   }
 
   function handleChangeRight(value) {
-    if(DEBUG)  console.log("Select right: ", value.value); // { value: "lucy", key: "lucy", label: "Lucy (101)" }
+    //if(DEBUG)  console.log("Select right: ", value.value); // { value: "lucy", key: "lucy", label: "Lucy (101)" }
     setRight(value.value);
   }
+
+  function onSliderChange(value) {
+    //if(DEBUG)  console.log("onSliderChange: ", value);
+    setSlider(value);
+
+  }
+
+
 
 
   return (
@@ -149,14 +182,18 @@ ${names[right]}  - 50%`);
            <Select
                labelInValue
                defaultValue={{ value: 6 }}
-               style={{ width: 150 }}
                onChange={handleChangeRight}
+               style={{ width: 150 , float: "right"}}
              >
             {optionsRight}
             </Select>
        </Col>
        </Row>
-
+       {disabled?(""):(
+       <div className="gx-mt-4">
+       Решите на какую бабочку должна быть больше похожа Ваша бабочка, передвинув слайдер:
+       <Slider defaultValue={slider} onChange={onSliderChange} disabled={disabled} />
+       </div>)}
 
       <div className="gx-mt-4"  style={{"whiteSpace": "pre-wrap"}}>
       <p>{description}</p>
