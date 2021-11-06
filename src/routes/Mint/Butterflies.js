9695e5678257b8d3e4012cb0c165982f134f2074
Benjamin Herald <bherald@pm.me>
2021-11-06 18:08:40 +0300

    CONTEXT

--- src/routes/Mint/Butterflies.js
+++ src/routes/Mint/Butterflies.js
@@ -7,7 +7,7 @@ import { metamaskLogin,
          virtuosoMint
          } from "../../blockchain/metamask";
 
-const {CONTEXT, REACT_APP_MINT_KEY} = process.env;
+const { REACT_APP_MINT_KEY} = process.env;
 const { addFileToIPFS, addToIPFS } = require("../../blockchain/ipfs");
 const {Meta} = Card;
 const { Option } = Select;
@@ -137,7 +137,7 @@ const MintButterfly = () => {
                  if( left !== right)
                  {
                         let path = "https://content.nftvirtuoso.io/image/butterflies/";
-                        if( CONTEXT === undefined) path = "/mintimages/butterflies/"
+                        //if( CONTEXT === undefined) path = "/mintimages/butterflies/"
                      		let image1 = await Jimp.read( path + left.toString() + ".png");
                      		// "https://content.nftvirtuoso.io/image/batterflies/5.jpg"
                      		setImageLeft(image1);
