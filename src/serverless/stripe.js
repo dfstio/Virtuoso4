d324f9e05f5fd41b7f526543c5895654013a8611
Benjamin Herald <bherald@pm.me>
2021-11-19 00:02:35 +0300

    winston meta 10

--- src/serverless/stripe.js
+++ src/serverless/stripe.js
@@ -3,109 +3,80 @@ const { URL,  STRIPE_KEY, STRIPE_ENDPOINT_SECRET, CHAIN_ID, CONTRACT_ADDRESS } =
 const stripe = require('stripe')(STRIPE_KEY);
 const delayMS = 1000;
 
+const logger  = require("./winston");
+const logm = logger.debug.child({ winstonModule: 'stripe' });
+
 
 const { addBalance, getTokenPrice } = require("./contract");
 const { lambdaTransferToken, lambdaAddBalance, lambdaMintItem } = require("../serverless/lambda");
-const DEBUG = ("true"===process.env.DEBUG);
-
 
 
 async function checkoutCompleted(body, headers)
 {
-        //if(DEBUG) console.log("checkoutCompleted ", body, headers, STRIPE_ENDPOINT_SECRET, STRIPE_KEY);
+
+        const log = logm.child({body, headers, wf: "checkoutCompleted"});
         const sig = headers['stripe-signature'];
         let endpointSecret = STRIPE_ENDPOINT_SECRET;
         if( (process.env.CONTEXT !== undefined) && (process.env.CONTEXT === 'dev') ) endpointSecret  = "whsec_z15BzPOir0nXvUcGGsavF2FuTr1diPQT";
         let event;
-        // if(DEBUG) console.log("checkoutCompleted start", endpointSecret, process.env.CONTEXT );
 
          try {
            event = stripe.webhooks.constructEvent(body, sig, endpointSecret);
          }
          catch (err) {
-           console.error(`Webhook Error: ${err}`);
+           log.error(`Webhook Error: ${err}`);
          }
 
-        if(DEBUG) console.log("checkoutCompleted event", event.type);
+        //log.info(`checkoutCompleted event: ${event.type}`);
          // Handle the event
          switch (event.type) {
-           case 'checkout.session.async_payment_succeeded':
-             const checkout = event.data.object;
-             console.log(`Webhook: checkout.session.async_payment_succeeded`);
-             // Then define and call a method to handle the successful payment intent.
-             // handlePaymentIntentSucceeded(paymentIntent);
-             break;
            case 'checkout.session.completed':
              const checkout1 = event.data.object;
-             //console.log(`Webhook: checkout.session.completed`);
              await handleCheckoutCompleted(checkout1);
-             // Then define and call a method to handle the successful payment intent.
-             // handlePaymentIntentSucceeded(paymentIntent);
-             break;
-           case 'charge.succeeded':
-             const charge = event.data.object;
-             //console.log(`Webhook: charge.succeeded`, charge);
-             //const customer = await stripe.customers.retrieve(charge.customer);
-             //console.log(`Webhook: customer `, customer);
-             //const paymentIntent = await stripe.paymentIntents.retrieve(charge.payment_intent);
-             //console.log(`Webhook: payment intent `, paymentIntent);
-             // Then define and call a method to handle the successful payment intent.
-             // handlePaymentIntentSucceeded(paymentIntent);
              break;
 
-
-           case 'checkout.session.async_payment_failed':
-             console.log(`Webhook: checkout.session.async_payment_failed`);
-             // Then define and call a method to handle the successful attachment of a PaymentMethod.
-             // handlePaymentMethodAttached(paymentMethod);
-             break;
            default:
              // Unexpected event type
-             console.log(`Webhook: Unhandled event type ${event.type}.`);
+             log.warn(`Webhook: Unhandled event type ${event.type}`);
          }
-
-         // Return a 200 response to acknowledge receipt of the event
-         //response.send();
-         //response.json({received: true});
-
-
 }
 
 async function handleCheckoutCompleted(checkout )
 {
-
+       const log = logm.child({checkout, wf: "handleCheckoutCompleted"});
+       //log.info(`processing: ${checkout.metadata.type}`);
 
     	 const paymentIntent = await stripe.paymentIntents.retrieve( checkout.payment_intent );
 
-       if(DEBUG) console.log("handleCheckoutCompleted: ", checkout.metadata.type, checkout.payment_status, paymentIntent.status);
+
        if( checkout.payment_status == 'paid')
        {
 
          switch (checkout.metadata.type) {
             case 'mint':
-            console.log("Mint: adding balance");
-            await lambdaAddBalance(checkout.metadata.address, 1000, "10 NFT mint pack bought");
+            log.error(`FEATURE REMOVED: Mint: adding balance to ${checkout.metadata.address}`);
+            //await lambdaAddBalance(checkout.metadata.address, 1000, "10 NFT mint pack bought");
             break;
             case 'buy':
-            console.log(`Buy`, checkout.metadata);
+            log.info(`Buy token ${checkout.metadata.tokenId} for ${checkout.customer_details.email}`);
             const id = parseInt(checkout.metadata.tokenId);
             await lambdaTransferToken(id, checkout.metadata,  checkout.customer_details.email );
             break;
             default:
             // Unexpected event type
-            console.log(`handleCheckoutCompleted: Unhandled event type ${checkout.metadata.type}.`);
+            log.error(`handleCheckoutCompleted: Unhandled event type ${checkout.metadata.type}`);
               }
        }
        else if( checkout.payment_status == 'unpaid' && checkout.metadata.type == 'buy' && paymentIntent.status == 'requires_capture')
        {
-         console.log("Checkout require capture - buy");
+         log.info(`Checkout require capture - buying token ${checkout.metadata.tokenId}`);
          const id = parseInt(checkout.metadata.tokenId);
          const status = await lambdaTransferToken(id, checkout.metadata,  checkout.customer_details.email );
          if( status)
          {
            const intent = await stripe.paymentIntents.capture(paymentIntent.id);
-           console.log("Payment captured: ", intent.status);
-         } else console.error("Payment NOT captured - token transfer failed");
+           //console.log("Payment captured: ", intent.status);
+         } else log.error("Payment NOT captured - token transfer failed");
 
        }
        else if( checkout.payment_status == 'unpaid' && checkout.metadata.type == 'mintItem' && paymentIntent.status == 'requires_capture')
@@ -115,8 +86,8 @@ async function handleCheckoutCompleted(checkout )
          if( status)
          {
            const intent = await stripe.paymentIntents.capture(paymentIntent.id);
-           console.log("Payment captured: ", intent.status);
-         } else console.error("Payment NOT captured - token minting failed");
+           //console.log("Payment captured: ", intent.status);
+         } else log.error("Payment NOT captured - token minting failed");
 
        };
 
@@ -127,7 +98,8 @@ async function handleCheckoutCompleted(checkout )
 
 async function createCheckoutSession(body)
 {
-  if(DEBUG) console.log("createCheckoutSession body", body);
+  //if(DEBUG) console.log("createCheckoutSession body", body);
+  const log = logm.child({body, wf: "createCheckoutSessio"});
 
   let success_url = URL + "/token/" + CHAIN_ID + "/" + CONTRACT_ADDRESS + "/" + body.tokenId.toString() + "/checkout/success";
 	let cancel_url  = URL + "/token/" + CHAIN_ID + "/" + CONTRACT_ADDRESS + "/" + body.tokenId.toString() + "/checkout/cancel";
@@ -189,7 +161,7 @@ async function createCheckoutSession(body)
 			 });
 			 return  session.url;
 		}
-		else console.error("Token No ", body.tokenId, " is not on sale" );
+		else log.error(`Token No ${body.tokenId} is not on sale`);
 	}
 	/*
 	else if(body.type == "mint" )
@@ -218,7 +190,7 @@ async function createCheckoutSession(body)
 	*/
 	else if(body.type == "mintItem" )
 	{
-	    	 console.log("MintItem order received", body);
+	    	 log.info(`MintItem order received for ${body.name}`);
 	       const session = await stripe.checkout.sessions.create({
                 payment_method_types: [
                 'card',
