ebe2171be061dfbc59e3325389a0300e60342228
Benjamin Herald <bherald@pm.me>
2021-11-20 20:24:55 +0300

    relay fix

--- src/functions/relay.js
+++ src/functions/relay.js
@@ -95,23 +95,24 @@ async function relay(relayData) {
 
 // Handler for lambda function
 exports.handler = async function(event, context, callback) {
-  try {
 
-    const data = JSON.parse(event.body);
-    logger.initMeta();
-    logger.meta.frontendMeta = body.winstonMeta;
-    logger.meta.frontendMeta.winstonHost = event.headers.host;
-    logger.meta.frontendMeta.winstonIP = event.headers['x-bb-ip'];
-    logger.meta.frontendMeta.winstonUserAgent = event.headers['user-agent'];
-    logger.meta.frontendMeta.winstonBrowser = event.headers['sec-ch-ua'];
 
-    //if(DEBUG) console.log("Relay function:", data);
-    const response = await relay(data);
-    await logger.flush();
-    callback(null, { statusCode: 200, body: JSON.stringify(response) });
+    const body = JSON.parse(event.body);
+    try {
+        logger.initMeta();
+        logger.meta.frontendMeta = body.winstonMeta;
+        logger.meta.frontendMeta.winstonHost = event.headers.host;
+        logger.meta.frontendMeta.winstonIP = event.headers['x-bb-ip'];
+        logger.meta.frontendMeta.winstonUserAgent = event.headers['user-agent'];
+        logger.meta.frontendMeta.winstonBrowser = event.headers['sec-ch-ua'];
+
+        //if(DEBUG) console.log("Relay function:", data);
+        const response = await relay(body);
+        await logger.flush();
+        callback(null, { statusCode: 200, body: JSON.stringify(response) });
   } catch (error) {
-    logm.error("catch", {error, body:event.body});
-    await logger.flush();
-    callback(error);
+        logm.error("catch", {error, body});
+        await logger.flush();
+        callback(error);
   }
 }
