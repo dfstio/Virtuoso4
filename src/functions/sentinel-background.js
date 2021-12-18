43bd17c157f0b20da379560fbde998de46152275
Benjamin Herald <bherald@pm.me>
2021-12-18 16:35:14 +0300

    sentnel

---
+++ src/functions/sentinel-background.js
@@ -0,0 +1,46 @@
+const { txBackground } = require("../serverless/contract");
+const logger  = require("../serverless/winston");
+const log = logger.info.child({ winstonModule: 'sentinel' });
+
+exports.handler = async(event, context) => {
+
+        // check for POST
+    if (event.httpMethod !== "POST") {
+        return {
+            statusCode: 400,
+            body: "sentinel: You are not using a http POST method for this endpoint.",
+            headers: { Allow: "POST" },
+        };
+    }
+
+    try {
+        const body = JSON.parse(event.body);
+        logger.initMeta();
+
+        log.info("sentinel", {body});
+        //await txBackground(body);
+        await logger.flush();
+
+                // return success
+        return {
+            statusCode: 200,
+            body: JSON.stringify({
+                success: true
+            }),
+        };
+
+
+    } catch (error) {
+
+       log.error("catch", {error, body:event.body});
+       await logger.flush();
+        // return error
+        return {
+            statusCode: error.statusCode || 500,
+            body: JSON.stringify({
+                message: error, success: false
+            }),
+        };
+    }
+
+};
